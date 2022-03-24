edbddd391c73812372b234794c46a60ea4219206
Keyushiru <inferngeek@gmail.com>
2022-03-24 20:49:44 +0300

    Added Picture class and table to database (c-8)

--- src/data/base/__init__.py
+++ src/data/base/__init__.py
@@ -1,14 +1,15 @@
 import sqlite3
-import os
 
-dbpath = os.getenv("DBURL")
-con = sqlite3.connect(dbpath)
+con = sqlite3.connect(r"data\base\main.db")
 cur = con.cursor()
 
 
 def init():
-    cur.execute('CREATE TABLE IF NOT EXISTS users (userid INT NOT NULL PRIMARY KEY, channel_tag CHAR(32), \
-    language CHAR(63))')
+    cur.execute('CREATE TABLE IF NOT EXISTS users (userid INT NOT NULL PRIMARY \
+    KEY, channel_tag CHAR(32), language CHAR(63))')
+    cur.execute('CREATE TABLE IF NOT EXISTS pictures (userid INT NOT NULL, \
+    file_id VARCHAR(255) NOT NULL, keyword CHAR(65), \
+    FOREIGN KEY (userid) REFERENCES userid (users))')
     con.commit()
 
 
@@ -23,8 +24,10 @@ class Users:
             self.__lan__ = row[2]
         else:
             self.__uid__ = userid
-            self.__lan__ = 'rus'
-            cur.execute('INSERT INTO users (userid, language) VALUES (?, ?)', (userid, 'rus'))
+            self.__lan__ = None
+            cur.execute(
+                'INSERT INTO users (userid, language) VALUES (?, ?)', (userid,
+                                                                       'rus'))
             con.commit()
 
     def __call__(self, value: str | None = None):
@@ -41,16 +44,44 @@ class Users:
             try:
                 tag = self.__tag__
             except AttributeError:
-                cur.execute('INSERT INTO users (userid, channel_tag) VALUES (?, ?)', (uid, value))
+                cur.execute(
+                    'INSERT INTO users (userid, channel_tag) VALUES (?, ?)',
+                    (uid, value))
             else:
-                cur.execute('UPDATE users SET channel_tag = ? WHERE userid = ?', (value, uid))
+                cur.execute(
+                    'UPDATE users SET channel_tag = ? WHERE userid = ?',
+                    (value, uid))
 
             con.commit()
 
     def lang(self, value: str | None = None):
         uid = self.__uid__
         if value:
-            cur.execute('UPDATE users SET language = ? WHERE userid = ?', (value, uid))
+            cur.execute(
+                'UPDATE users SET language = ? WHERE userid = ?', (value, uid))
             con.commit()
         else:
             return self.__lan__
+
+
+class Picture:
+    def __init__(self, user_id, keyword=None, file_id=None):
+        self.__d = [user_id, keyword]
+        if file_id:
+            cur.execute('INSERT INTO pictures (userid, file_id, keyword) \
+                    VALUES (?, ?, ?)', (user_id, file_id, keyword))
+            con.commit()
+        else:
+            cur.execute('SELECT file_id FROM pictures WHERE userid = ? AND keyword = ?',
+                        (user_id, keyword))
+            self.__r = cur.fetchall()
+
+    def get(self):
+        return self.__r
+
+    def set(self, file_id, keyword=None):
+        kw = keyword if keyword else self.__d[1]
+        user_id = self.__d[0]
+        cur.execute('INSERT INTO pictures (userid, file_id, keyword) \
+                VALUES (?, ?, ?)', (user_id, file_id, kw))
+        con.commit()
