21e8a3e11bc45abc264aadf318a8b4b73455a1b3
Keyushiru <inferngeek@gmail.com>
2022-03-24 21:11:02 +0300

    language (c-13)

--- src/data/languages.py
+++ src/data/languages.py
@@ -2,10 +2,8 @@ text = {
     "rus": {
         "already_specified": "Вы уже указали канал для бота!",
         "first_message": "Здравствуйте! Для начала работы выберите язык (доступные: /rus, /eng) \n" +
-                         "Hello! For the beginning choose language (available: /rus, /eng) " +
-                         "Или перешлите сообщение из канала, который будет использоваться " +
-                         "для поиска картинок.",
-        "second_message": "Присылайте картинки (по одной или по нескольку) указывая ключевые слова. "
+                         "Hello! For the beginning choose language (available: /rus, /eng) ",
+        "second_message": "Присылайте картинки (по одной) указывая ключевые слова. "
                           "Но если вы не собираетесь указывать ключевые слова, ничего страшного! В "
                           "дальнейшем такие картинки можно будет найти по ключевому слову #w (также "
                           "его можно изменить в настройках /settings)",
@@ -13,10 +11,10 @@ text = {
     },
     "eng": {
         "already_specified": "You already specified the channel for bot!",
-        "first_message": "Hello! For the beginning choose language (available: /rus, /eng) " +
-                         "or forward message from channel, which you will use for searching pictues.",
-        "second_message": "Send me pictures with their keywords. But if you don't wanna link pictures with keyword, "
+        "first_message": "Hello! For the beginning choose language (available: /rus, /eng) ",
+        "second_message": "Send me picture with his keywords. But if you don't wanna link pictures with keyword, "
                           "you can do this. In the future that pictures could find by keyword #w (and it could edit "
-                          "in /settings) "
+                          "in /settings) ",
+        "invalid_input": """Send at least one picture!"""
     }
 }
