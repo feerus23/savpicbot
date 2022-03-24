4dc304b7388b8efc71b12ccc36a7b90ea732b761
Keyushiru <inferngeek@gmail.com>
2022-03-24 20:51:33 +0300

    Added capture of pictures handler and lang files for it (c-10)

--- src/data/languages.py
+++ src/data/languages.py
@@ -1,14 +1,22 @@
 text = {
     "rus": {
         "already_specified": "Вы уже указали канал для бота!",
-        "first_message": "Здравствуйте! Для начала работы выберите язык (if you wanna change " +
-                         "language pls send me /%ur_lang%, available languages: /rus, /eng) " +
-                         "если вам это надо, если нет, то отметьте канал через @, из которого бот " +
-                         "будет брать картиночки."
+        "first_message": "Здравствуйте! Для начала работы выберите язык (доступные: /rus, /eng) \n" +
+                         "Hello! For the beginning choose language (available: /rus, /eng) " +
+                         "Или перешлите сообщение из канала, который будет использоваться " +
+                         "для поиска картинок.",
+        "second_message": "Присылайте картинки (по одной или по нескольку) указывая ключевые слова. "
+                          "Но если вы не собираетесь указывать ключевые слова, ничего страшного! В "
+                          "дальнейшем такие картинки можно будет найти по ключевому слову #w (также "
+                          "его можно изменить в настройках /settings)",
+        "invalid_input": """Отправьте хотя бы одну картинку!"""
     },
     "eng": {
         "already_specified": "You already specified the channel for bot!",
         "first_message": "Hello! For the beginning choose language (available: /rus, /eng) " +
-                         "or specify channel from whole we are take pictures "
+                         "or forward message from channel, which you will use for searching pictues.",
+        "second_message": "Send me pictures with their keywords. But if you don't wanna link pictures with keyword, "
+                          "you can do this. In the future that pictures could find by keyword #w (and it could edit "
+                          "in /settings) "
     }
 }
--- src/handlers/message.py
+++ src/handlers/message.py
@@ -1,18 +1,40 @@
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.types import Message
-from data.base import Users
+from data.base import Users, Picture
 from data.languages import text
+from handlers.states import States
+from data import bot
 
 
 async def start(m: Message):
     u = Users(m.from_user.id)
     lang = u.lang()
 
-    if u():
-        await m.answer(text[lang]['already_specified'])
+    if lang is None:
+        await m.answer(text['rus']['first_message'])
     else:
-        await m.answer(text[lang]['first_message'])
+        await m.answer(text[lang]['second_message'])
+        await States.beginning.set()
+
+
+async def on_picture_capture(m: Message):
+    uid = m.from_user.id
+    lang = Users(uid).lang()
+
+    if m.caption:
+        kw = m.caption if len(m.caption) > 0 else "%w"
+        print(len(m.photo), m.photo)
+
+        if len(m.photo) > 0:
+            n = 0
+            for p in m.photo:
+                if n % 4 == 0:
+                    Picture(uid, kw, p.file_id)
+                n += 1
+    else:
+        await m.answer(text[lang]['invalid_input'])
 
 
 def reg(dp: Dispatcher):
     dp.register_message_handler(start)
+    dp.register_message_handler(on_picture_capture, state=States.beginning, content_types=['photo', 'text'])
