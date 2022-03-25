d7b22ab567839a6c68230c73615f524b76a33687
Keyushiru <inferngeek@gmail.com>
2022-03-25 20:03:23 +0300

    Minor fixes (c-25)

--- src/handlers/command.py
+++ src/handlers/command.py
@@ -1,4 +1,4 @@
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.types import Message
 from data.base import Users
 from . import message
@@ -11,10 +11,9 @@ cmds = {
 
 async def change_language(m: Message):
     t = m.text.replace('/', '')
-
     await m.answer('Done!')
-
     Users(m.from_user.id).lang(t)
+    return
 
 
 async def settings(m: Message):
