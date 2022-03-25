bf56ac1626df22b572ffaca3e9a47ca27550b957
Keyushiru <inferngeek@gmail.com>
2022-03-25 20:07:27 +0300

    III point of plans implementation (c-25)

--- src/handlers/message.py
+++ src/handlers/message.py
@@ -14,22 +14,37 @@ async def start(m: Message):
         await m.answer(text['rus']['first_message'])
     else:
         await m.answer(text[lang]['second_message'])
-        await States.on_pic_cap.set()
+        await States.on_pic_cap0.set()
 
 
-async def on_picture_capture(m: Message):
+async def on_picture_capture(m: Message, state: FSMContext):
     uid = m.from_user.id
-    lang = Users(uid).lang()
+    u = Users(uid)
+    lang = u.lang()
 
     kw = m.caption if m.caption else "#w"
-    print(len(m.photo), m.photo)
 
     if len(m.photo) > 0:
         Picture(uid, kw, m.photo[-1].file_id)
     else:
-        await m.answer(text[lang]['invalid_input'])
+        await m.answer(text[lang]['send_photo'])
+        await state.update_data(keyword=m.text)
+        await States.on_pic_cap1.set()
+
+
+async def on_picture_capture_1(m: Message, state: FSMContext):
+    uid = m.from_user.id
+    u = Users(uid)
+    lang = u.lang()
+    an = await state.get_data()
+
+    if len(m.photo) > 0:
+        Picture(uid, an['keyword'], m.photo[-1].file_id)
+    else:
+        await m.answer(text[lang]["invalid_input"])
 
 
 def reg(dp: Dispatcher):
     dp.register_message_handler(start)
-    dp.register_message_handler(on_picture_capture, state=States.on_pic_cap, content_types=['photo', 'text'])
+    dp.register_message_handler(on_picture_capture, state=States.on_pic_cap0, content_types=['photo', 'text'])
+    dp.register_message_handler(on_picture_capture_1, state=States.on_pic_cap1, content_types=['photo', 'text'])
--- src/handlers/states.py
+++ src/handlers/states.py
@@ -2,4 +2,5 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class States(StatesGroup):
-    on_pic_cap = State()
+    on_pic_cap0 = State()
+    on_pic_cap1 = State()
