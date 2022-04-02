5242db6513c89d77f4b8c323052ab98bc4a36c73
Keyushiru <inferngeek@gmail.com>
2022-04-02 18:40:03 +0300

    Bug fix

--- src/handlers/inline.py
+++ src/handlers/inline.py
@@ -11,18 +11,24 @@ def get_result(p: Picture, start_num):
     if start_num >= overall_items:
         return []
     elif start_num + 50 >= overall_items:
-        return res[start_num:overall_items]
+        return res[start_num-1:overall_items]
     else:
-        return res[start_num:start_num + 50]
+        return res[start_num-1:start_num + 50]
 
 
 async def request(q: InlineQuery):
     query_offset = int(q.offset) if q.offset else 1
 
     t = q.query
+    try:
+        if t[0] == '#':
+            t = t.replace('#', '', 1)
+    except IndexError:
+        pass
+
     uid = q.from_user.id
     lang = Users(uid).lang()
-    res = get_result(Picture(uid, keyword=t.replace('#', '', 1) if t[0] == '#' else t), q.offset)
+    res = get_result(Picture(uid, keyword=t), query_offset)
 
     photos = [InlineQueryResultCachedPhoto(id=item[0][:50], photo_file_id=item[0]) for item in res]
 
