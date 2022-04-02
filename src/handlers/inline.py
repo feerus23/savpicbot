3cabfc01290ca6c795ab4bb48f3490c6d4625838
Keyushiru <inferngeek@gmail.com>
2022-04-02 17:10:39 +0300

    Теперь можно листать сохраненки если у тебя их больше 50!

--- src/handlers/inline.py
+++ src/handlers/inline.py
@@ -1,27 +1,47 @@
 from aiogram.dispatcher import Dispatcher, FSMContext
-from aiogram.types import Message, InlineQuery, InlineQueryResultCachedPhoto
+from aiogram.types import InlineQuery, InlineQueryResultCachedPhoto
 from data.base import Users, Picture
 from data.languages import text
-from data import bot
+
+
+def get_result(p: Picture, start_num):
+    res = p()
+    overall_items = len(res)
+
+    if start_num >= overall_items:
+        return []
+    elif start_num + 50 >= overall_items:
+        return res[start_num:overall_items]
+    else:
+        return res[start_num:start_num + 50]
 
 
 async def request(q: InlineQuery):
+    query_offset = int(q.offset) if q.offset else 1
+
     t = q.query
     uid = q.from_user.id
     lang = Users(uid).lang()
-    res = Picture(uid, keyword=t)()
+    res = get_result(Picture(uid, keyword=t.replace('#', '', 1) if t[0] == '#' else t), q.offset)
 
-    if len(res) == 0:
-        if '#' in t:
-            t.replace('#', '')
-        else:
-            t = '#' + t
-        res = Picture(uid, keyword=t)()
+    photos = [InlineQueryResultCachedPhoto(id=item[0][:50], photo_file_id=item[0]) for item in res]
 
-    photos = [InlineQueryResultCachedPhoto(id=item[0][:64], photo_file_id=item[0]) for item in res]
-
-    await q.answer(photos, cache_time=60, is_personal=True,
-                   switch_pm_text=text[lang]["add_pic"], switch_pm_parameter="picadd")
+    if len(photos) < 50:
+        await q.answer(photos,
+                       cache_time=60,
+                       is_personal=True,
+                       switch_pm_text=text[lang]["add_pic"],
+                       switch_pm_parameter="picadd",
+                       next_offset=''
+                       )
+    else:
+        await q.answer(photos,
+                       cache_time=60,
+                       is_personal=True,
+                       switch_pm_text=text[lang]["add_pic"],
+                       switch_pm_parameter="picadd",
+                       next_offset=str(query_offset + 15)
+                       )
 
 
 def reg(dp: Dispatcher):
